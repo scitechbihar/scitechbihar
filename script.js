3747c294f5292d7d353da562fef42c6740184f6e
scitechbihar <a38997689@gmail.com>
2026-03-13 13:59:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function toggleMenu(){
     nav.classList.toggle("active");
     burger.classList.toggle("active");
 }
+
+<script>
+function searchLetters() {
+
+let input = document.getElementById("searchInput");
+let filter = input.value.toLowerCase();
+let letters = document.getElementsByClassName("letter");
+
+for (let i = 0; i < letters.length; i++) {
+
+let text = letters[i].textContent || letters[i].innerText;
+
+if (text.toLowerCase().indexOf(filter) > -1) {
+letters[i].style.display = "";
+} else {
+letters[i].style.display = "none";
+}
+
+}
+
+}
+
+</script>
