bf61f1317d807c987fbdbd5aba7ea3f24067e98b
scitechbihar <a38997689@gmail.com>
2026-03-17 06:39:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ nav.classList.toggle("active");
 }
 
 
-
-function toggleSearch(){
-const box = document.getElementById("searchBox");
-box.classList.toggle("active");
+function toggleSearch() {
+    const searchBox = document.getElementById("searchBox");
+    searchBox.classList.toggle("active");
+    
+    // अगर सर्च बॉक्स खुल जाए, तो ऑटोमैटिक कर्सर वहां चला जाए
+    if (searchBox.classList.contains("active")) {
+        searchBox.querySelector("input").focus();
+    }
 }
 
 
